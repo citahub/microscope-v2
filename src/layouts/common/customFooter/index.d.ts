64ac67469b0a96e8b0e3d21ff088f5e100115328
wu <wuyuyue@cryptape.com>
2019-03-06 16:15:23 +0800

    chore: remove unnecessary files

--- src/layouts/common/customFooter/index.d.ts
+++
@@ -1,6 +0,0 @@
-import  React from 'react';
-import './index.styl';
-declare class CustomFooter extends React.Component<any, any> {
-    render(): JSX.Element;
-}
-export default CustomFooter;
--- src/layouts/common/transactionTable/index.d.ts
+++
@@ -1,9 +0,0 @@
-import  React from 'react';
-import './index.styl';
-import 'rc-pagination/assets/index.css';
-import 'rc-select/assets/index.css';
-declare class TransactionTable extends React.Component<any, any> {
-    componentDidMount(): void;
-    render(): JSX.Element;
-}
-export default TransactionTable;

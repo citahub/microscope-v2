d9ab0dfe3bcf68298f772c366fc94318adc2975c
wu <wuyuyue@cryptape.com>
2019-06-28 19:00:46 +0800

    fix: add service-worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,11 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //   .BundleAnalyzerPlugin
 const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
-
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
@@ -15,7 +15,14 @@ module.exports = merge(common, {
     }),
     new CopyPlugin([{ from: 'public/' }]),
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
-
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'microscope-2.0.0',
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      minify: true,
+      navigateFallback: 'index.html',
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+    }),
     new HtmlWebpackPlugin({
       template: 'public/index.html',
       minify: true,
